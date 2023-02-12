67d7209646653564ab9a1924fa0bb0fd5a8545fa
DiegoArcelli <diegoarcelli99@gmail.com>
2023-02-12 18:08:54 +0100

    added decoder

--- src/models.py
+++ src/models.py
@@ -16,21 +16,66 @@ class BartForNMT(nn.Module):
         return output
         
 
-
+'''
+Encoder of the seq2seq model
+It uses and Embedding layer to map the words of each input sentence to vector
+and then the embedding are passed to a Bidirectional GRU to produce 
+the context vector for the decoder
+'''
 class Encoder(nn.Module):
 
-    def __init__(self, input_dim, hidden_dim, output_dim, n_layers) -> None:
+    '''
+    - input_dim: size of the vocabulary
+    - hidden_dim: size of the embedding
+    - output_dim: size of the output of the GRU
+    '''
+    def __init__(self, input_dim, hidden_dim, n_layers) -> None:
         super(Encoder, self).__init__()
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
-        self.output_dim = output_dim
         self.n_layers = n_layers
         self.embedder = nn.Embedding(input_dim, hidden_dim)
-        self.encoder = nn.GRU(hidden_dim, output_dim, n_layers, bidirectional=True)
+        self.encoder = nn.GRU(hidden_dim, hidden_dim, n_layers, bidirectional=False)
 
 
     def forward(self, x):
         x = self.embedder(x)
         out, hidden = self.encoder(x)
         return out, hidden
-        
+    
+
+
+class AttentionLayer(nn.Module):
+
+    
+    def __init__(self, embedded_dim, n_heads) -> None:
+        super(AttentionLayer, self).__init__()
+        self.mha = nn.MultiheadAttention(embedded_dim, n_heads)
+
+    def forward(self, input, context):
+        out, weights = self.mha(context, input, input)
+        return out, weights
+
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, vocab_dim, hidden_dim, n_layers, n_heads) -> None:
+        super(Decoder, self).__init__()
+        self.input_dim = vocab_dim
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        self.n_heads = n_heads
+
+        self.embedder = nn.Embedding(vocab_dim, hidden_dim)
+        self.decoder = nn.GRU(hidden_dim, hidden_dim, n_layers, bidirectional=False)
+        self.attention = AttentionLayer(hidden_dim, n_heads)
+        self.output = nn.Linear(hidden_dim, vocab_dim)
+
+    def forward(self, x, context):
+        x = self.embedder(x)
+        x, state = self.decoder(x)
+        out, weights = self.attention(x, context)
+        logits = self.output(out)
+
+        return logits, state
--- src/test.py
+++ src/test.py
@@ -1,25 +1,23 @@
-from models import BartForNMT, Encoder
-from transformers import AutoTokenizer
 import torch
+from torch import nn
+import torch.functional as F
+from models import Decoder, Encoder
 
-# tokenizer = AutoTokenizer.from_pretrained("facebook/bart-base")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# sentence = ["I love my cute dog",
-#             "I hate black cats",
-#             "I eat candies all day",
-#             "I don't want to set the world on fire"
-#             ]
+vocab_size = 5000
+batch_size = 100
 
-# # model = BartForNMT(768, 32768)
+x = torch.randint(0, 5000, (batch_size, 20))
+y = torch.randint(0, 5000, (batch_size, 20))
 
-# inputs = tokenizer(sentence, return_tensors="pt", max_length=20, padding='max_length').input_ids
-# print(inputs.shape)
-# output = model(inputs.input_ids)
-# print(output.keys())
-x = torch.randint(0, 5000, (100, 20))
-enc = Encoder(5000, 256, 128, 5)
-y, h = enc(x)
+enc = Encoder(vocab_size, 256, 1)
+dec = Decoder(vocab_size, 256, 1, 1)
 
-print("Input: ", x.shape)
-print("Output: ", y.shape)
-print("Hidden: ", h.shape)
+c, _ = enc(x)
+
+print("Encoder output", c.shape)
+
+logits, state = dec(y, c)
+print("Decoder state: ", state.shape)
+print("Decoder output: ", logits.shape)
