2de0aa1367f25e5368c99d62599bd645c6e2a65d
DiegoArcelli <diegoarcelli99@gmail.com>
2023-08-21 13:01:04 +0200

    added padded lables

--- src/models/modeling_prompt_mt5.py
+++ src/models/modeling_prompt_mt5.py
@@ -1771,6 +1771,15 @@ class MT5ForConditionalGeneration(MT5PreTrainedModel):
 
         loss = None
         if labels is not None:
+
+            if labels is not None:
+                batch_size, labels_num_tokens = labels.shape
+                lm_logits_num_tokens = lm_logits.shape[1]
+                if labels_num_tokens != lm_logits_num_tokens:
+                    pad_tokens_num = lm_logits_num_tokens - labels_num_tokens
+                    soft_prompts_indices = torch.full((batch_size, pad_tokens_num), -100).to(self.device)
+                    labels = torch.concat([soft_prompts_indices, labels], dim=1)
+
             loss_fct = CrossEntropyLoss(ignore_index=-100)
             loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1))
             # TODO(thom): Add z_loss https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/layers.py#L666
