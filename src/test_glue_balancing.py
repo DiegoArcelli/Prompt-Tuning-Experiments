067f96b6344ef5023656d020430f4026ce989e8c
DiegoArcelli <diegoarcelli99@gmail.com>
2023-08-29 13:20:07 +0200

    added tokens count

--- src/test_glue_balancing.py
+++ src/test_glue_balancing.py
@@ -1,8 +1,11 @@
 from datasets import load_dataset
 import numpy as np
+from transformers import AutoTokenizer
+from glue_config import glue_config
 
 for task in ["cola", "mrpc", "qnli", "qqp", "rte", "sst2", "wnli"]:
     print(task)
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
     dataset = load_dataset("glue", task)
     print(dataset.keys())
     train_set = dataset["train"]
@@ -17,5 +20,15 @@ for task in ["cola", "mrpc", "qnli", "qqp", "rte", "sst2", "wnli"]:
     class_1_valid = sum(valid_labels)
     class_0_valid = tot_valid - class_1_valid
     print(f"Train class 0: {class_0_train} ({class_0_train/tot_train}), class 1: {class_1_train} ({class_1_train/tot_train})")
-    print(f"Valid class 0: {class_0_valid} ({class_0_valid/tot_valid}), class 1: {class_1_valid} ({class_1_valid/tot_valid})\n\n")
+    print(f"Valid class 0: {class_0_valid} ({class_0_valid/tot_valid}), class 1: {class_1_valid} ({class_1_valid/tot_valid})")
+
+    for key in glue_config[task]:
+        attr = glue_config[task][key]
+        if key != "num_to_text":
+            _list = list(map(lambda x: x[attr], train_set))
+            m = max([len(tokenizer(x, max_length=None).input_ids) for x in _list])
+            print(attr, m)
+
+    print("\n\n")
+
 
